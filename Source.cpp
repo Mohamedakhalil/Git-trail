751f88f5a30397871aa8e07ff7688010a3ecec1b
Mohamedakhalil <mohamedakhalil@aucegypt.edu>
2020-09-08 16:01:39 +0200

    khalil's edits

--- Source.cpp
+++ Source.cpp
@@ -22,13 +22,13 @@ int knapSack(int W, int wt[], int val[], int n)
 // main 
 // Mohamed Nasr was here :)
 // Mahmoud Hassan
-int main()
+// Mohamed Ali
+void main()
 {
-	int val[] = { 70, 130, 120, 50 };
+	int val[] = { 70, 130, 18, 50 };
 	int wt[] = { 10, 20, 30 };
 	int W = 70;
 	int n = sizeof(val) / sizeof(val[0]);
 	cout << knapSack(W, wt, val, n);
-	return 0;
 }
 // This code is contributed by rathbhupendra
